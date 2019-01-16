562f792b5e81dc8b05b054d00d6735a02c18a04c
Andy <andy.kleinhesselink@gmail.com>
2019-01-16 15:02:20 -0800

    Fix species life history and life form

--- data-raw/species_list_functions.R
+++ data-raw/species_list_functions.R
@@ -5,7 +5,7 @@ get_species_list <- function(){
   url <- 'http://www.calflora.org/entry/wgh.html#srch=t&group=none&fmt=photo&inma=t&y=34.6927&x=-120.042&z=12&lpstr=t&lpom=d'
 
   species_list <- 'data-raw/raw_species_list.txt'
-  naat <- 'data-raw/navarretia_atractyloides.txt'
+  naat <- 'data-raw/navarretia_atractyloides.txt'  # I add these to the species list to be consistent with lab
   erbo <- 'data-raw/erodium_botrys.txt'
 
   species <-
@@ -14,7 +14,8 @@ get_species_list <- function(){
     mutate( calflora_binomial =
               str_replace_all(calflora_taxon, c('var\\..*' = '', 'ssp\\..*'=''))) %>%
     mutate( calflora_binomial = str_trim(calflora_binomial)) %>%
-    separate( lifeform, c('life_history', 'form')) %>%
+    mutate( life_history = str_extract(lifeform, '^[A-Za-z]+')) %>%
+    mutate( form = str_extract( str_extract(lifeform, '[\\(A-Za-z\\)]+$'), '[A-Za-z]+'))  %>%
     mutate( status = str_extract(status, c('^native|non-native'))) %>%
     select( -bloom)
 
